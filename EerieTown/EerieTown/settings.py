6c4d9d4912c48b94dcd9da3cf0c44b93d1b102f1
uldiniumGit <uldiniumGit@yandex.ru>
2023-08-11 19:42:26 +0500

    1234

--- EerieTown/EerieTown/settings.py
+++ EerieTown/EerieTown/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+# TODO 1
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
