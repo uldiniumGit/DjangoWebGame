3316d88354310e7fc1c1163d2b3ce5a8565b3649
uldiniumGit <114669125+uldiniumGit@users.noreply.github.com>
2025-06-01 16:51:00 +0700

    Update views.py

--- EerieTown/siteapp/views.py
+++ EerieTown/siteapp/views.py
@@ -1,114 +1,104 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, CreateView, UpdateView, View
-from .models import PatchNote, GameUser
-from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView, CreateView
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import GameUser
+from django.contrib.auth.views import LoginView
 from .forms import RegistrationForm, LoginForm
-from django.http import HttpResponse, HttpResponseRedirect
+
+
+# Универсальный класс для установки булевого флага пользователя и редиректа
+class SetUserFlagView(LoginRequiredMixin, View):
+    flag_name = None  # Имя поля у пользователя
+    flag_value = True
+    redirect_url = 'siteapp:index'
+
+    def get(self, request):
+        if not self.flag_name:
+            return redirect(self.redirect_url)
+        setattr(request.user, self.flag_name, self.flag_value)
+        request.user.save()
+        return redirect(self.redirect_url)
 
 
 # Ключи
-class GetMasterKeyView(View):
+class GetMasterKeyView(LoginRequiredMixin, View):
     def get(self, request):
-        user = request.user
-        setattr(user, 'has_forge_key', True)
-        setattr(user, 'has_greenhouse_key', True)
-        setattr(user, 'has_stones_key', True)
-        setattr(user, 'has_loft_key', True)
-        setattr(user, 'has_kitchen_key', True)
-        setattr(user, 'has_fisherman_key', True)
-        user.save()
+        keys = [
+            'has_forge_key',
+            'has_greenhouse_key',
+            'has_stones_key',
+            'has_loft_key',
+            'has_kitchen_key',
+            'has_fisherman_key'
+        ]
+        for key in keys:
+            setattr(request.user, key, True)
+        request.user.save()
         return redirect('siteapp:index')
 
 
-class LoseMasterKeyView(View):
+class LoseMasterKeyView(LoginRequiredMixin, View):
     def get(self, request):
-        user = request.user
-        setattr(user, 'has_forge_key', False)
-        setattr(user, 'has_greenhouse_key', False)
-        setattr(user, 'has_stones_key', False)
-        setattr(user, 'has_loft_key', False)
-        setattr(user, 'has_kitchen_key', False)
-        setattr(user, 'has_fisherman_key', False)
-        setattr(user, 'day_two', False)
-        setattr(user, 'day_three', False)
-        setattr(user, 'end', False)
-        user.save()
+        keys_to_reset = [
+            'has_forge_key',
+            'has_greenhouse_key',
+            'has_stones_key',
+            'has_loft_key',
+            'has_kitchen_key',
+            'has_fisherman_key',
+            'day_two',
+            'day_three',
+            'end'
+        ]
+        for key in keys_to_reset:
+            setattr(request.user, key, False)
+        request.user.save()
         return redirect('siteapp:index')
 
 
-class GetForgeKeyView(View):
-    def get(self, request):
-        user = request.user
-        setattr(user, 'has_forge_key', True)
-        user.save()
-        return redirect('siteapp:index')
+class GetForgeKeyView(SetUserFlagView):
+    flag_name = 'has_forge_key'
 
 
-class GetGreenhouseKeyView(View):
-    def get(self, request):
-        user = request.user
-        setattr(user, 'has_greenhouse_key', True)
-        user.save()
-        return redirect('siteapp:index')
+class GetGreenhouseKeyView(SetUserFlagView):
+    flag_name = 'has_greenhouse_key'
 
 
-class GetStonesKeyView(View):
-    def get(self, request):
-        user = request.user
-        setattr(user, 'has_stones_key', True)
-        user.save()
-        return redirect('siteapp:fisherman')
+class GetStonesKeyView(SetUserFlagView):
+    flag_name = 'has_stones_key'
+    redirect_url = 'siteapp:fisherman'
 
 
-class GetLoftKeyView(View):
-    def get(self, request):
-        user = request.user
-        setattr(user, 'has_loft_key', True)
-        user.save()
-        return redirect('siteapp:hall')
+class GetLoftKeyView(SetUserFlagView):
+    flag_name = 'has_loft_key'
+    redirect_url = 'siteapp:hall'
 
 
-class GetKitchenKeyView(View):
-    def get(self, request):
-        user = request.user
-        setattr(user, 'has_kitchen_key', True)
-        user.save()
-        return redirect('siteapp:index')
+class GetKitchenKeyView(SetUserFlagView):
+    flag_name = 'has_kitchen_key'
 
 
-class GetFishermanKeyView(View):
-    def get(self, request):
-        user = request.user
-        setattr(user, 'has_fisherman_key', True)
-        user.save()
-        return redirect('siteapp:index')
+class GetFishermanKeyView(SetUserFlagView):
+    flag_name = 'has_fisherman_key'
 
 
 # Смена дня
-class DayTwoView(View):
-    def get(self, request):
-        user = request.user
-        setattr(user, 'day_two', True)
-        user.save()
-        return redirect('siteapp:loft')
+class DayTwoView(SetUserFlagView):
+    flag_name = 'day_two'
+    redirect_url = 'siteapp:loft'
 
 
-class DayThreeView(View):
-    def get(self, request):
-        user = request.user
-        setattr(user, 'day_three', True)
-        user.save()
-        return redirect('siteapp:greenhouse')
+class DayThreeView(SetUserFlagView):
+    flag_name = 'day_three'
+    redirect_url = 'siteapp:greenhouse'
 
 
 # Конец
-class EndView(View):
-    def get(self, request):
-        user = request.user
-        setattr(user, 'end', True)
-        user.save()
-        return redirect('siteapp:square')
+class EndView(SetUserFlagView):
+    flag_name = 'end'
+    redirect_url = 'siteapp:square'
 
 
 # Вспомогательные страницы
